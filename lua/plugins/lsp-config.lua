7823b6ea9bf6dfb8e3294882033f6519d9c1cf74
Felipe Brito <felipe.correia.wd@gmail.com>
2024-05-19 13:25:32 -0300

    Configurações para o neovim compativeis com o iTerm2

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+---@diagnostic disable: undefined-global
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      autoinstall = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
+
